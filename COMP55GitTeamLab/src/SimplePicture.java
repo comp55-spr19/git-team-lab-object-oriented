680b416388e13261b586b50e594e23e77c932782
hromiti <hromiti@STK-ECS-2KWFRP2.stk.pacific.edu>
2019-03-19 13:49:43 -0700

    rekt

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -1,9 +1,6 @@
 //comment one
-<<<<<<< HEAD
 //Hayden's comment
-=======
 //Colton was here
->>>>>>> branch 'master' of https://github.com/comp55-spr19/git-team-lab-object-oriented.git
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;

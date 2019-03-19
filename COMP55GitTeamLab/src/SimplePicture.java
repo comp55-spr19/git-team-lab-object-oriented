3ba4bd7eabb37d4a5374b701769f4ea96a5aea5d
rdamerel <rdamerel@STK-ECS-2L0JRP2.stk.pacific.edu>
2019-03-19 14:41:45 -0700

    changed

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -17,7 +17,7 @@ public class SimplePicture extends GraphicsProgram {
 	}
 	//E10
 	public void run() {
-		GImage robot = new GImage("robot.jpg", 200, 100);
+		GImage robot = new GImage("robert.jpg", 200, 100);
 		add(robot);
 		GLabel label = new GLabel("MFW I change the label again!", 200, 400);
 	

fc31e4f5e2b2c80f2126868d6d73f4e94e6cc583
hromiti <hromiti@STK-ECS-2KWFRP2.stk.pacific.edu>
2019-03-19 14:48:31 -0700

    fuix

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -9,25 +9,14 @@ import ac\Label;
 import acm.pro
 public clastends GraphicsProgram {
 	public static int SIZE = 700;
-	
-<<<<<<< HEAD
 	public void initetSize(SIZE, SIZE);
-=======
+
 	public void init() {
 		setSize(SIZE, SIZE); //les go
->>>>>>> branch 'master' of https://github.com/comp55-spr19/git-team-lab-object-oriented.git
+
 	}
 	//E10
-<<<<<<< HEAD
-	pubun() {
 
-		GImage robot = ne", 200, 100);
-		add(robot);
-=======
-	public void run() {
-		GImage robert = new GImage("robert.jpg", 200, 100);
-		add(robert);
->>>>>>> branch 'master' of https://github.com/comp55-spr19/git-team-lab-object-oriented.git
 		GLabel label = new GLabel("Github is ok", 200, 400);
 
 		//obj

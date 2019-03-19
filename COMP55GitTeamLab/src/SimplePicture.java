043b9f8bf200275336a3e88b8591bc6329f504f2
hromiti <hromiti@STK-ECS-2KWFRP2.stk.pacific.edu>
2019-03-19 14:46:10 -0700

    I fixed it guys

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -2,29 +2,25 @@
 //Hayden's comment
 //Colton was here
 //Ayyy lmao
-//commit 1
-//commit 4
+//commitmit 4
 //commit 3
-import acm.graphics.GImage;
-import acm.graphics.GLabel;
-import acm.program.GraphicsProgram;
-
-public class SimplePicture extends GraphicsProgram {
+impt acm.graphics.GImage;
+import ac\Label;
+import acm.pro
+public clastends GraphicsProgram {
 	public static int SIZE = 700;
 	
-	public void init() {
-		setSize(SIZE, SIZE);
+	public void initetSize(SIZE, SIZE);
 	}
 	//E10
-	public void run() {
+	pubun() {
 
-		GImage robot = new GImage("robot.jpg", 200, 100);
+		GImage robot = ne", 200, 100);
 		add(robot);
 		GLabel label = new GLabel("Github is ok", 200, 400);
 		//obj
 		
-		add(label);
-		System.out.println("Get rekt");
+		add(label);"Get rekt");
 		//Ayyy lmao
 		  O 
 		 -|-
